a976fa79bdcbf3a3c1159f6fb6602e7f299facb3
Ramiro <ramirosales52@gmail.com>
2023-03-31 03:37:49 -0300

    html terminado

--- portfolio-angular/src/app/app.module.ts
+++ portfolio-angular/src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
+import { XpComponent } from './components/xp/xp.component';
+import { EduComponent } from './components/edu/edu.component';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectComponent } from './components/proyect/proyect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    XpComponent,
+    EduComponent,
+    HysComponent,
+    ProyectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ portfolio-angular/src/app/components/xp/xp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-xp',
+  templateUrl: './xp.component.html',
+  styleUrls: ['./xp.component.css']
+})
+export class XpComponent {
+
+}
